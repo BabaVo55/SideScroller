4854fab344fc87fe2c9e49c1754abbcee548a697
BabaVo55 <ethemcode@outlook.com>
2025-03-18 14:35:50 +0000

    movement cancel

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class Player {
         }
         this.velocity = {
             x: 0,
-            y: 1
+            y: 0
         }
         this.width = 100;
         this.height = 100;
@@ -28,10 +28,22 @@ class Player {
 
     update(){
             this.draw()
-            // while (this.position.y <= 729){
-                this.position.y += this.velocity.y  
+
+            this.position.x += this.velocity.x;
+            this.position.y += this.velocity.y;
+
+            if (this.position.x +  this.width <= 0 && 
+                this.position.x + this.width >= canvas.width
+            ){
+                this.velocity.x = 0;
+            }
+
+            if (this.position.y + this.velocity.y + this.height <= canvas.height){
+                this.position.y += this.velocity.y;
                 this.velocity.y += gravity
-            // }
+            } else {
+                this.velocity.y = 0;
+            }
     }
 }
 
@@ -41,6 +53,13 @@ const player = new Player()
 //     console.log(event.pageY)
 // })
 
+// let info = addEventListener('keydown', (event) => {
+//     console.log(event)
+// })
+
+
+
+
 function animate(){
     requestAnimationFrame(animate)
     c.clearRect(0,0, canvas.width, canvas.height)
@@ -48,4 +67,41 @@ function animate(){
 }
 
 
+
 animate()
+
+window.addEventListener('keydown', (event) => {
+    // if (event.key === 'd'){
+    //     this.position.x + 1;
+    // }
+    switch(event.key){
+        case 'a':
+            player.velocity.x += 1;
+            break;
+        case 'd':
+            player.velocity.x -= 1;
+            break;
+        case 'w': 
+            player.velocity.y += -20;
+            break
+    }
+    
+})
+window.addEventListener('keyup', (event) => {
+    if (event.key === 'd'){
+        player.velocity.x = 0;
+    } 
+    if (event.key === 'a'){
+        player.velocity.x = 0;
+    }
+
+    // switch(event.key){
+    //     case 'a':
+    //         player.velocity.x = 0;
+    //         break;
+    //     case 'd':
+    //         player.velocity.x = 0;
+    //         break;
+    // }
+    
+})
