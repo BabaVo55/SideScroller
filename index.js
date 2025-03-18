72188c1451d8f9a6b72a42f5da4b1cab54a3aa63
BabaVo55 <ethemcode@outlook.com>
2025-03-18 13:55:23 +0000

    position.y velocity.y + gravity

--- index.js
+++ index.js
@@ -4,21 +4,48 @@ const c = canvas.getContext('2d');
 
 canvas.height = window.innerHeight
 canvas.width = window.innerWidth
+const gravity = .8;
 
 class Player {
-    constructor(){
+    constructor(color='blue'){
         this.position = {
             x : 100,
             y : 100
         }
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
         this.width = 100;
         this.height = 100;
+        this.color = color;
     }
 
     draw(){
+        c.fillStyle = this.color
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update(){
+            this.draw()
+            // while (this.position.y <= 729){
+                this.position.y += this.velocity.y  
+                this.velocity.y += gravity
+            // }
     }
 }
 
 const player = new Player()
-player.draw()
+
+// let info = addEventListener('mousemove', (event) => {
+//     console.log(event.pageY)
+// })
+
+function animate(){
+    requestAnimationFrame(animate)
+    c.clearRect(0,0, canvas.width, canvas.height)
+    player.update()
+}
+
+
+animate()
